3c836c93f2f1c1d915b7a10144b3fdbd3eb4c1b4
Lakshya <harshitlucky2004@gmail.com>
2024-11-27 00:20:36 +0530

    Vercel checks

--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -8,7 +8,7 @@ const SignInPage = () => {
 
   const handleSignIn = async () => {
     try {
-      const response = await fetch('http://localhost:8080/user/login', {
+      const response = await fetch('/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
